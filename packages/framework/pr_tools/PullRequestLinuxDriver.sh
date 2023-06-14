9cae43109844ffbd5db2269571f56c6388e798bc
Justin M. LaPre <jmlapre@sandia.gov>
2023-06-14 09:09:00 -0600

    try aue/cmake/3.24.3

--- packages/framework/pr_tools/PullRequestLinuxDriver.sh
+++ packages/framework/pr_tools/PullRequestLinuxDriver.sh
@@ -47,7 +47,7 @@ function bootstrap_modules() {
         get_python_packages pip3
         export PYTHON_EXE=python3
     elif [[ ${on_cts1} == "1" ]]; then
-        module load aue/cmake/3.26.2
+        module load aue/cmake/3.24.3
         module load aue/git/2.38.1
         module load aue/python/3.9.16
         get_python_packages pip3
