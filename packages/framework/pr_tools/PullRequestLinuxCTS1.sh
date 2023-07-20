1e72beabf230a6587a61e87b2453792e775e9f04
Samuel Browne <sebrown@sandia.gov>
2023-07-20 10:24:15 -0600

    Use a batch script to allow for more commands
    User Support Ticket(s) or Story Referenced: TRILFRAME-567

--- packages/framework/pr_tools/PullRequestLinuxCTS1.sh
+++ packages/framework/pr_tools/PullRequestLinuxCTS1.sh
@@ -17,7 +17,14 @@ fi
 stdout=job_stdout.log
 stderr=job_stderr.log
 
-sbatch --output=${stdout} --error=${stderr} --wait --account=fy200165 --exclusive --partition=short --ntasks=16 --job-name="${JOB_NAME}" --time="${SLURM_CTEST_TIME_LIMIT}" "TMPDIR=/dev/shm/trilinos mkdir ${TMPDIR} && \"${WORKSPACE}\"/Trilinos/packages/framework/pr_tools/PullRequestLinuxDriver.sh --on_cts1"
+mybatchscript=$(pwd)/batch_${PPID}.sh
+echo "#!/bin/bash" > ${mybatchscript}
+echo "TMPDIR=/dev/shm/trilinos" >> ${mybatchscript}
+echo "mkdir ${TMPDIR}" >> ${mybatchscript}
+echo "\"${WORKSPACE}\"/Trilinos/packages/framework/pr_tools/PullRequestLinuxDriver.sh --on_cts1" >> ${mybatchscript}
+chmod ug+x ${mybatchscript}
+
+sbatch --output=${stdout} --error=${stderr} --wait --account=fy200165 --exclusive --partition=short --ntasks=16 --job-name="${JOB_NAME}" --time="${SLURM_CTEST_TIME_LIMIT}" ${mybatchscript}
 ESTAT=$?
 
 echo -e "\n\nJob stdout is as follows\n\n"
