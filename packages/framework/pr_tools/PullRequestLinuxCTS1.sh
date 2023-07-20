e076e10c2560fc246102d7acf01473944dc02cb3
Samuel Browne <sebrown@sandia.gov>
2023-07-20 09:56:22 -0600

    Put /tmp in /dev/shm
    /tmp is small and seems to not work well with the Intel compiler,
    so move stuff to /dev/shm/trilinos
    
    User Support Ticket(s) or Story Referenced: TRILFRAME-567

--- packages/framework/pr_tools/PullRequestLinuxCTS1.sh
+++ packages/framework/pr_tools/PullRequestLinuxCTS1.sh
@@ -17,7 +17,7 @@ fi
 stdout=job_stdout.log
 stderr=job_stderr.log
 
-sbatch --output=${stdout} --error=${stderr} --wait --account=fy200165 --exclusive --partition=short --ntasks=16 --job-name="${JOB_NAME}" --time="${SLURM_CTEST_TIME_LIMIT}" "${WORKSPACE}"/Trilinos/packages/framework/pr_tools/PullRequestLinuxDriver.sh --on_cts1
+sbatch --output=${stdout} --error=${stderr} --wait --account=fy200165 --exclusive --partition=short --ntasks=16 --job-name="${JOB_NAME}" --time="${SLURM_CTEST_TIME_LIMIT}" "TMPDIR=/dev/shm/trilinos mkdir ${TMPDIR} && \"${WORKSPACE}\"/Trilinos/packages/framework/pr_tools/PullRequestLinuxDriver.sh --on_cts1"
 ESTAT=$?
 
 echo -e "\n\nJob stdout is as follows\n\n"
